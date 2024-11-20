208127a290fd886197b49c75284c0f7f3764a9e7
Migs <migspajarillo19@gmail.com>
2024-11-21 01:25:21 +0800

    Initial commit

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const emit = defineEmits(['transactionDeleted'])
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+});
+
+// Delete transaction
+const deleteTransaction = (id) => {
+    emit('transactionDeleted', id);
+}
+</script>
+
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="transaction in transactions" :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>₱ {{ transaction.amount }}</span>
+            <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
+        </li>
+    </ul>
+</template>
